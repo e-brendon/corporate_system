55e2786f60a43141f5eb434ba88ed4c72cce7ced
Brendon Esteves <brendonmontteiro@gmail.com>
2025-10-19 19:22:15 -0400

    .env ajusts

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,8 @@
 # from pages import models
 
+import os
+
+
 def context_social(request):
     user = getattr(request, 'user', None)
     can_manage_users = False
@@ -8,4 +11,5 @@ def context_social(request):
     return {
         'social': 'Exibir este contexto em qualquer lugar!',
         'can_manage_users': can_manage_users,
+        'app_name': os.getenv('APP_NAME', 'e-brendon'),
     }

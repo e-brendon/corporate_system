d4d9595fbc256b119fc328d6f541cbcf0c13e6ef
Brendon Esteves <brendonmontteiro@gmail.com>
2025-10-31 19:50:22 -0400

    atualizacao_user

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from forum.forms import PostagemForumForm
+from django.contrib import messages  
 from forum import models
+from base.utils import add_form_errors_to_messages
 # Create your views here.
 
 def lista_postagem_forum(request):
     postagens = models.PostagemForum.objects.filter(ativo=True)
     context = {'postagens': postagens}
     return render(request, 'lista-postagens-forum.html', context)
+
+# Cria postagens 
+def criar_postagem_forum(request):
+    form = PostagemForumForm()
+    if request.method == 'POST':
+        form = PostagemForumForm(request.POST, request.FILES)
+        if form.is_valid():
+            forum = form.save(commit=False)
+            forum.usuario = request.user
+            form.save()
+            # Redirecionar para uma página de sucesso ou fazer qualquer outra ação desejada
+            messages.success(request, 'Seu Post foi cadastrado com sucesso!')
+            return redirect('lista-postagem-forum')
+        else:
+            add_form_errors_to_messages(request, form)
+            
+    return render(request, 'form-postagem-forum.html', {'form': form})
+
